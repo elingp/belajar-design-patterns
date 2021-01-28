3c3b4bdbbd1810a1a7cca37aa1f8780564253461
Eling Pramuatmaja <elingpramuatmaja@gmail.com>
2021-01-28 11:46:21 +0800

    Factory design pattern

--- src/main/java/com/blibli/belajar/design/patterns/singleton/SingletonApplication.java
+++ src/main/java/com/blibli/belajar/design/patterns/singleton/SingletonApplication.java
@@ -1,11 +1,13 @@
 package com.blibli.belajar.design.patterns.singleton;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 public class SingletonApplication {
 
+  @SpringBootApplication
   public static class Configuration {
 
     @Bean
