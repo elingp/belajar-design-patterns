06d383d016dbfa03e183f39414058c5bbcf74ebd
Eling Pramuatmaja <elingpramuatmaja@gmail.com>
2021-01-28 16:45:17 +0800

    Dependency injection pattern

---
+++ src/main/java/com/blibli/belajar/design/patterns/di/DependencyinjectionApplication.java
@@ -0,0 +1,55 @@
+package com.blibli.belajar.design.patterns.di;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+public class DependencyinjectionApplication {
+
+  @SpringBootApplication
+  public static class Application {
+
+    // @Bean
+    // public Foo foo() {
+    // return new Foo();
+    // }
+
+    @Component
+    @Data
+    public static class Foo {
+
+      // @Autowired
+      private Bar bar;
+
+      public Foo(Bar bar) {
+        this.bar = bar;
+      }
+    }
+
+    @Component
+    @Data
+    public static class Bar {
+
+      // @Autowired
+      private Foo foo;
+
+      public Bar(Foo foo) {
+        this.foo = foo;
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    ApplicationContext context = SpringApplication.run(Application.class);
+
+    Application.Foo foo = context.getBean(Application.Foo.class);
+    Application.Bar bar = context.getBean(Application.Bar.class);
+
+    System.out.println(foo == bar.getFoo());
+  }
+
+}
